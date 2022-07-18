14bb55da47a76248e06bca15e1edc70bf3ee9736
ad4mx <fleischlingeradam@seznam.cz>
2022-07-18 10:46:33 +0200

    fix: edited gif

--- examples/all_spinners.rs
+++ examples/all_spinners.rs
@@ -5,7 +5,7 @@ use strum::IntoEnumIterator;
 fn main() {
     let mut spin = Spinner::new(Spinners::Dots, "", None);
     for spinner in Spinners::iter() {
-        spin.update(spinner, format!("Spinners::{}", spinner), None);
+        spin.update(spinner, format!("{}", spinner), None);
         sleep(Duration::from_secs(2));
     }
     spin.stop_with_message("Done!");
